d35cf9775cbea60e91f595dc48beaa49a6c0348d
JackE772 <jackewing772@gmail.com>
2025-12-02 15:59:30 -0500

    moved some code around all chill

--- DAQ_main/dataProssesing/GUI/main.py
+++ DAQ_main/dataProssesing/GUI/main.py
@@ -1,5 +1,6 @@
 import sys
 from sideBar import Sidebar
+from GPSDisplay import GPS_Window
 from PySide6.QtCore import Qt
 from PySide6.QtWidgets import QApplication, QMainWindow, QHBoxLayout, QWidget, QPushButton, QVBoxLayout, QSplitter
 
@@ -9,6 +10,22 @@ from PySide6.QtWidgets import QApplication, QMainWindow, QHBoxLayout, QWidget, Q
 #highlight: #E3F8F6
 #boarders: #562D8B
 class MainWindow(QMainWindow):
+
+    spliter_syle = """
+            QSplitter {
+                background-color: #0E1935;
+            }
+            QSplitter::handle:horizontal {
+                background-color: #562D8B;
+                width: 2px;
+                margin: 2px;
+            }
+            QSplitter::handle:vertical {
+                background-color: #562D8B;
+                height: 2px;
+                margin: 2px;
+            }
+        """
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Data Processor")
@@ -19,30 +36,23 @@ class MainWindow(QMainWindow):
         layout = QVBoxLayout(central_widget)
 
         splitter = QSplitter(Qt.Horizontal)
-        splitter.setStyleSheet("""
-            QSplitter {
-                background-color: #0E1935;  /* same as window bg */
-            }
-            QSplitter::handle {
-                background-color: #562D8B;  /* slightly lighter for subtle contrast */
-                width: 5px;                 /* thin flat line */
-                margin: 5px;
-            }
-            """)
+        splitter.setStyleSheet(self.spliter_syle)
         layout.addWidget(splitter)
 
         self.sidebar = Sidebar()
+        self.sidebar.setMinimumWidth(100)
+        self.sidebar.setMaximumWidth(600)
         splitter.addWidget(self.sidebar)
         #self.sidebar.button_clicked.connect(self.handle_sidebar_click)
 
+        middleSpliter = QSplitter(Qt.Vertical)
         content = QWidget()
         content_layout = QVBoxLayout(content)
         content_layout.addWidget(QPushButton("Button 1"))
-        content_layout.addWidget(QPushButton("Button 2"))
-        splitter.addWidget(content)
-
-        self.sidebar.setMinimumWidth(100)
-        self.sidebar.setMaximumWidth(600)
+        middleSpliter.addWidget(QPushButton("Button 2"))
+        middleSpliter.addWidget(content)
+        middleSpliter.setSizes([100, 300])
+        splitter.addWidget(middleSpliter)
 
 
 def setupWindow():
